87d4629761ae07df1c0fefe67cfb1d6561bdf1d6
aglc-dennis <Dennis.Mah@aglc.ca>
2014-03-26 10:03:06 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,9 +1,14 @@
-﻿namespace app
+﻿using System;
+
+namespace app
 {
   public class Calculator
   {
     public int add(int first, int second)
     {
+        if (first < 0 || second < 0)
+            throw new ArgumentException("I can't add negative numbers :(");
+
         return first + second;
     }
   }
