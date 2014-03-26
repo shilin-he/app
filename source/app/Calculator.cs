db085a97c443eb245cef002b21174d2cfa46f9ce
jreyes20 <jason.reyes@aglc.ca>
2014-03-26 10:21:53 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -6,10 +6,12 @@ namespace app
   public class Calculator
   {
     IDbConnection connection;
+    IDbCommand command;
 
     public Calculator(IDbConnection connection)
     {
       this.connection = connection;
+      this.command = connection.CreateCommand();
     }
 
     public int add(int first, int second)
@@ -18,6 +20,7 @@ namespace app
         throw new ArgumentException("I can't add negative numbers :(");
 
       connection.Open();
+      command.ExecuteNonQuery();
       return first + second;
     }
   }
