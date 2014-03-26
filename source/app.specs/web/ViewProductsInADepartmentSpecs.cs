8a4ee1c60a4c43a11fdc9bff5e0aa39b849f7f94
Max He <shilin.he@gmail.com>
2014-03-26 15:33:48 -0600

    Pushing new changes

---
+++ source/app.specs/web/ViewProductsInADepartmentSpecs.cs
@@ -0,0 +1,50 @@
+﻿ using System.Collections.Generic;
+ using app.web.application.catalogbrowsing;
+ using app.web.core;
+ using Machine.Specifications;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+ using Rhino.Mocks.Constraints;
+
+namespace app.specs.web
+{  
+  [Subject(typeof(ViewProductsInADepartment))]  
+  public class ViewProductsInADepartmentSpecs
+  {
+    public abstract class concern : Observes<IImplementAUserStory,
+      ViewProductsInADepartment>
+    {
+        
+    }
+   
+    public class when_run : concern
+    {
+      Establish c = () =>
+      {
+        the_department = new DepartmentLineItem();
+        the_products = new List<ProductLineItem>
+        {
+          new ProductLineItem()
+        };
+
+        request = fake.an<IProvideDetailsAboutARequest>();
+        request.setup(x => x.map<DepartmentLineItem>()).Return(the_department);
+        display_engine = depends.on<IDisplayInformation>();
+        products = depends.on<IFindProducts>();
+        products.setup(x => x.find_products_in(the_department)).Return(the_products);
+      };
+
+      Because b = () =>
+        sut.process(request);
+
+      It display_products_in_the_department = () =>
+        display_engine.received(x => x.display(the_products));
+
+      static IProvideDetailsAboutARequest request;
+      static IDisplayInformation display_engine;
+      static DepartmentLineItem the_department;
+      static IEnumerable<ProductLineItem> the_products;
+      static IFindProducts products;
+    }
+  }
+}
---
+++ source/app/web/application/catalogbrowsing/IFindProducts.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace app.web.application.catalogbrowsing
+{
+  public interface IFindProducts
+  {
+    IEnumerable<ProductLineItem> find_products_in(DepartmentLineItem the_department);
+  }
+}
