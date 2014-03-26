da606b94a7c238b8e4e18c0ccd16e02c2964b6bd
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-03-26 12:42:41 -0600

    Updated

--- source/app.specs/web/CommandRegistrySpecs.cs
+++ source/app.specs/web/CommandRegistrySpecs.cs
@@ -16,7 +16,6 @@ namespace app.specs.web
         
     }
 
-   
     public class when_getting_a_command_to_process_a_request : concern
     {
       public class and_it_has_the_command
@@ -42,6 +41,32 @@ namespace app.specs.web
 
         static IProcessOneRequest result;
         static IProcessOneRequest the_command_that_can_process_the_request;
+        static IProvideDetailsAboutARequest request;
+        static IList<IProcessOneRequest> all_commands;
+      }
+      public class and_it_does_not_have_the_command 
+      {
+        Establish c = () =>
+        {
+          all_commands = Enumerable.Range(1, 100).Select(x => fake.an<IProcessOneRequest>()).ToList();
+          special_case = fake.an<IProcessOneRequest>();
+          request = fake.an<IProvideDetailsAboutARequest>();
+          depends.on<IEnumerable<IProcessOneRequest>>(all_commands);
+          depends.on<ICreateASpecialCaseWhenACommandIsNotFound>(x =>
+          {
+            x.ShouldEqual(request);
+            return special_case;
+          });
+        };
+
+        Because b = () =>
+          result = sut.get_command_that_can_process(request);
+
+        It return_the_result_of_running_the_special_case_handler = () =>
+          result.ShouldEqual(special_case);
+
+        static IProcessOneRequest result;
+        static IProcessOneRequest special_case;
         static IProvideDetailsAboutARequest request;
         static IList<IProcessOneRequest> all_commands;
       }
