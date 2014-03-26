a07f2eb8829db5a6bb77096fa2c406118f9105b0
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-03-26 10:09:08 -0600

    Updated

--- source/app.specs/CalculatorSpecs.cs
+++ source/app.specs/CalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using Machine.Specifications;
@@ -15,15 +16,26 @@ namespace app.specs
     {
       public class two_positive_numbers
       {
+        Establish c = () =>
+        {
+          connection = depends.on<IDbConnection>();   
+        };
+
         //Act
         Because b = () =>
           result = sut.add(2, 3);
 
         //Assert
+
+        It opens_a_connection_to_the_database = () =>
+          connection.received(x => x.Open());
+
         It returns_the_sum = () =>
           result.ShouldEqual(5);
 
+
         static int result;
+        static IDbConnection connection;
       }
 
       public class a_negative_and_a_positive
