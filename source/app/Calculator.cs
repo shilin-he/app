b5378da1827e5811dabbaf6eed94dc9afec089e3
Max He <shilin.he@gmail.com>
2014-03-26 10:14:52 -0600

    Pushing new changes

--- source/app/Calculator.cs
+++ source/app/Calculator.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Data;
 
 namespace app
 {
   public class Calculator
   {
+    IDbConnection connection;
+
+    public Calculator(IDbConnection connection)
+    {
+      this.connection = connection;
+    }
+
     public int add(int first, int second)
     {
         if (first < 0 || second < 0)
             throw new ArgumentException("I can't add negative numbers :(");
 
+      connection.Open();
         return first + second;
     }
   }
