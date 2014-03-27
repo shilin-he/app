ebc8c30b89a52d3f8a1b71bd5a01ddee2318a064
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-03-27 13:59:12 -0600

    Updated

---
+++ source/app.specs/utility/SimpleDependencyFactorySpecs.cs
@@ -0,0 +1,38 @@
+﻿ using System;
+ using System.Data.SqlClient;
+ using app.utility.container.basic;
+ using Machine.Specifications;
+ using developwithpassion.specifications.rhinomocks;
+ using developwithpassion.specifications.extensions;
+
+namespace app.specs.utility
+{  
+  [Subject(typeof(SimpleDependencyFactory))]  
+  public class SimpleDependencyFactorySpecs
+  {
+    public abstract class concern : Observes<ICreateOneDependency,
+      SimpleDependencyFactory>
+    {
+        
+    }
+
+   
+    public class when_it_creates_a_dependency : concern
+    {
+      Establish c = () =>
+      {
+        the_result = new SqlConnection();
+        depends.on<Func<object>>(() => the_result);
+      };
+
+      Because b = () =>
+        result = sut.create();
+
+      It returns_the_object_created_by_its_provided_delegate = () =>
+        result.ShouldEqual(the_result);
+
+      static object result;
+      static SqlConnection the_result;
+    }
+  }
+}
--- source/app/utility/container/basic/DependencyFactories.cs
+++ source/app/utility/container/basic/DependencyFactories.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace app.utility.container.basic
@@ -7,15 +6,19 @@ namespace app.utility.container.basic
   public class DependencyFactories : IFindFactoriesThatCanCreateDependencies
   {
     IDictionary<Type, ICreateOneDependency> factories;
+    ICreateADependencyFactoryWhenItIsMissing special_case;
 
-    public DependencyFactories(IDictionary<Type, ICreateOneDependency> factories)
+    public DependencyFactories(IDictionary<Type, ICreateOneDependency> factories, ICreateADependencyFactoryWhenItIsMissing special_case)
     {
       this.factories = factories;
+      this.special_case = special_case;
     }
 
     public ICreateOneDependency get_factory_that_can_create(Type dependency_type)
     {
-      return factories[dependency_type];
+      if (factories.ContainsKey(dependency_type)) return factories[dependency_type];
+
+      return special_case(dependency_type);
     }
   }
 }
