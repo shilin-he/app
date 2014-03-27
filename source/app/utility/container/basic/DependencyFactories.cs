75ca6ad8a40920987ecc4c3f4f78d72f3f46f329
Max He <shilin.he@gmail.com>
2014-03-27 13:52:52 -0600

    Pushing new changes

--- source/app/utility/container/basic/DependencyFactories.cs
+++ source/app/utility/container/basic/DependencyFactories.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace app.utility.container.basic
 {
   public class DependencyFactories : IFindFactoriesThatCanCreateDependencies
   {
+    IDictionary<Type, ICreateOneDependency> factories;
+
+    public DependencyFactories(IDictionary<Type, ICreateOneDependency> factories)
+    {
+      this.factories = factories;
+    }
+
     public ICreateOneDependency get_factory_that_can_create(Type dependency_type)
     {
-      throw new NotImplementedException();
+      return factories[dependency_type];
     }
   }
 }
