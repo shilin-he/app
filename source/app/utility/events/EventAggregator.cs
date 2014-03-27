a3555e9bca6456f38d497aaf0b790f4aec584ff5
Max He <shilin.he@gmail.com>
2014-03-27 09:16:20 -0600

    Pushing new changes

--- source/app/utility/events/EventAggregator.cs
+++ source/app/utility/events/EventAggregator.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace app.utility.events
 {
   public class EventAggregator
   {
+    public EventAggregator(IDictionary<string, List<object>> handlers)
+    {
+      this.handlers = handlers;
+    }
+
+    IDictionary<string, List<object>> handlers;
+
     public void register_listener(string ring, Delegate our_handler)
     {
-      throw new System.NotImplementedException();
+      if (!handlers.Keys.Contains(ring)) handlers[ring] = new List<object>();
+      var ring_handlers = handlers[ring];
+      if (!ring_handlers.Contains(our_handler)) ring_handlers.Add(our_handler);
     }
   }
 }
