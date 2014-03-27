92b21cdb764233bd42b037702b1b883c712bd690
Max He <shilin.he@gmail.com>
2014-03-27 13:19:17 -0600

    Pushing new changes

--- source/app/utility/container/basic/Container.cs
+++ source/app/utility/container/basic/Container.cs
@@ -1,18 +1,29 @@
-﻿namespace app.utility.container.basic
+﻿using System;
+
+namespace app.utility.container.basic
 {
   public class Container : IFetchDependencies
   {
     IFindFactoriesThatCanCreateDependencies factories;
+    ICreateDependencyCreationErrors error_factory;
 
-    public Container(IFindFactoriesThatCanCreateDependencies factories)
+    public Container(IFindFactoriesThatCanCreateDependencies factories, ICreateDependencyCreationErrors error_factory)
     {
       this.factories = factories;
+      this.error_factory = error_factory;
     }
 
     public Dependency an<Dependency>()
     {
-      var factory = factories.get_factory_that_can_create(typeof(Dependency));
-      return (Dependency)factory.create();
+      try
+      {
+        var factory = factories.get_factory_that_can_create(typeof(Dependency));
+        return (Dependency)factory.create();
+      }
+      catch (Exception e)
+      {
+        throw error_factory(typeof(Dependency), e);
+      }
     }
   }
 }
